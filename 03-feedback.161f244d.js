da2a6fa46ff25cb8904cb7a28de94df3a232e154
Alexandr-Styopin <79509688+Alexandr-Styopin@users.noreply.github.com>
2022-11-24 00:05:15 +0000

    Deploying to gh-pages from @ Alexandr-Styopin/goit-js-hw-08@3e2291f73e85cf613eb398eea04530c041ca69db 🚀

---
+++ 03-feedback.161f244d.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form");console.log("dfccccc");
+//# sourceMappingURL=03-feedback.161f244d.js.map
---
+++ 03-feedback.7317f66d.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form");console.log("dfccccc")}();
+//# sourceMappingURL=03-feedback.7317f66d.js.map
