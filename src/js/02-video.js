5133a52a73389663c123f511a75df7d057788858
Sandroo <styopin.alexandr@gmail.com>
2022-11-27 17:29:18 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
-console.log("object-vimeo");
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+}, 1000));
+
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+};
